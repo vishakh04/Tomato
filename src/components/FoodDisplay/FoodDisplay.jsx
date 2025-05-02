7d9bb4579ece65a0f99bbc6674106af4ae6b159f
Vishakh Singh <120209997+vishakh04@users.noreply.github.com>
2025-05-02 13:43:16 +0530

    Update FoodDisplay.jsx

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,7 +9,7 @@ const FoodDisplay = ({category}) => {
 
   return (
     <div className='food-display' id='food-display'>
-      <h2>Top dishes near you</h2>
+      <h2>Top dishes near you aaaa</h2>
       <div className='food-display-list'>
         {food_list.map((item)=>{
           if (category==="All" || category===item.food_category) {
